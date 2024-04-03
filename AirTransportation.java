356cba81493f8a38e834d5d927fb57254c508238
monteiroRui <202300265@estudantes.ips.pt>
2024-04-03 11:22:25 +0100

    Level 3

--- AirTransportation.java
+++ AirTransportation.java
@@ -9,6 +9,7 @@ public class AirTransportation extends Transport
 {
     private String name;
     private int numberOfContainers;
+    private int price;
     private static final double FEE = 4.0;
     public AirTransportation(String name, int numberOfContainers){
         super();
@@ -30,5 +31,28 @@ public class AirTransportation extends Transport
 
     public void setNumberOfContainers(int numberOfContainers) {
         this.numberOfContainers = numberOfContainers;
+    }
+    
+    @Override
+    public double getPriceWithFees(){
+        return this.price * (1.0 + (FEE * 0.01)); 
+    }
+    
+    @Override
+    public String getTransportType(){
+        return "Transporte aéreo";
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Nome", getName())); 
+        sb.append(String.format("\n%15s: %s\n", "Tipo de transporte", getTransportType()));  
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", this.FEE));
+        sb.append(String.format("%15s: %4.2f%%\n", "Número de containers", getNumberOfContainers()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+     
+        return sb.toString();
     }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -5,9 +5,10 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class GroundTransportation
+public class GroundTransportation extends Transport
 {
     private String licensePlate;
+    private int price;
     private static final double FEE = 3.0;
     public GroundTransportation(String licensePlate)
     {
@@ -21,5 +22,27 @@ public class GroundTransportation
 
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
+    }
+    
+    @Override
+    public double getPriceWithFees(){
+        return this.price * (1.0 + (FEE * 0.01)); 
+    }
+    
+    @Override
+    public String getTransportType(){
+        return "Transporte terrestre";
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("\n%15s: %s\n", "Matrícula", getLicensePlate())); 
+        sb.append(String.format("\n%15s: %s\n", "Tipo de transporte", getTransportType()));  
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", this.FEE));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", getPriceWithFees()));
+     
+        return sb.toString();
     }
 }
