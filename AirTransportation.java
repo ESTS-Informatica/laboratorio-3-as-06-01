8bca5d711af695eab9aab454995b6bc9597baa72
monteiroRui <202300265@estudantes.ips.pt>
2024-04-03 11:00:24 +0100

    Level 1

---
+++ AirTransportation.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class AirTransportation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AirTransportation extends Transport
+{
+    private String name;
+    private int numberOfContainers;
+    private static final double FEE = 4.0;
+    public AirTransportation(){
+        
+    }
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class GroundTransportation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GroundTransportation
+{
+    private String licensePlate;
+    private static final double FEE = 3.0;
+    public GroundTransportation()
+    {
+        super();
+    }
+}
